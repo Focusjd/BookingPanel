71cf5955fd3f1102746b14c7d1d3091ef9fb19ec
Focus <8004930-FooxKing@users.noreply.gitlab.com>
2021-05-10 15:46:16 +0800

    Project init

--- src/BookingPanel.java
+++ src/BookingPanel.java
@@ -1,21 +1,2 @@
-import javax.swing.*;
-import java.awt.*;
-
-public class BookingPanel extends JFrame
-{
-    private JPanel panel1;
-    //    public BookingPanel()
-//    {
-//        setTitle("Java 第一个 GUI 程序");    //设置显示窗口标题
-//        setSize(400,200);    //设置窗口显示尺寸
-//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    //置窗口是否可以关闭
-//        JLabel jl=new JLabel("这是使用JFrame类创建的窗口");    //创建一个标签
-//        Container c=getContentPane();    //获取当前窗口的内容窗格
-//        c.add(jl);    //将标签组件添加到内容窗格上
-//        setVisible(true);    //设置窗口是否可见
-//    }
-//    public static void main(String[] agrs)
-//    {
-//        new BookingPanel();    //创建一个实例化对象
-//    }
+public class BookingPanel {
 }
