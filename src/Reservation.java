df5be4f8ad1cb5b0c3b4bd5aad7caae0a737e6fb
Focus <8004930-FooxKing@users.noreply.gitlab.com>
2021-05-10 15:33:38 +0800

    project_init

---
+++ src/Reservation.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class Reservation {
+    private String firstName;
+    private String lastName;
+    private Date startDate;
+    private Date endDate;
+    private int customerNum;
+    private int phone;
+
+
+
+}
