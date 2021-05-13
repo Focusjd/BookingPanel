9dd33ab13091308a8215ed6bf38a58967f425006
Focus <8004930-FooxKing@users.noreply.gitlab.com>
2021-05-14 00:35:26 +0800

    add image for panel, implement basic framework

--- src/BookingPanel.java
+++ src/BookingPanel.java
@@ -6,6 +6,9 @@ public class BookingPanel extends JFrame{
     private JPanel mapPanel;
     private JTextPane titleText;
     private JPanel selectionPanel;
+    private JPanel divisionLine;
+
+    private JScrollPane scrollPane;
 
     public BookingPanel(String title, int w, int h, int x, int y){
         super(title);
@@ -17,25 +20,37 @@ public class BookingPanel extends JFrame{
         this.setSize(w,h);
         this.setLocation(x,y);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(true);
 //        this.pack(); //this will change the frame size according to the current frame component
         this.setVisible(true);
-
     }
 
     private void initComponent(){
         this.setLayout(new BorderLayout());
         mapPanel = new JPanel();
         selectionPanel = new JPanel();
+        divisionLine =new JPanel();
 
         mapPanel.setBackground(Color.pink);
+//        scrollPane = new JScrollPane(setImg("src/map.png"),ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS,ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        mapPanel.setPreferredSize(new Dimension(1000, 100));
         mapPanel.add(setImg("src/map.png"));
+        JScrollPane scrollPane = new JScrollPane(mapPanel);
+        scrollPane.scrollRectToVisible(new Rectangle(0,0,100,100));
+        this.add(scrollPane);
+//        this.add(mapPanel);
+
+        divisionLine.setBackground(Color.ORANGE);
+        divisionLine.setSize(5,this.getHeight());
+//        divisionLine.setBounds(mapPanel.getWidth(),0,this.getWidth()/20,this.getHeight());
 
         selectionPanel.setLayout(new BoxLayout(selectionPanel, BoxLayout.PAGE_AXIS));
         selectionPanel.add(setTitleText("Focusing Hotel Booking System"));
 
 
+//        this.add(divisionLine, BorderLayout.CENTER);
         this.add(mapPanel, BorderLayout.WEST);
-        this.add(selectionPanel, BorderLayout.EAST);
+        this.add(selectionPanel, BorderLayout.CENTER);
 //        this.add(new JButton("center"), BorderLayout.CENTER);
 
     }
@@ -51,6 +66,7 @@ public class BookingPanel extends JFrame{
         JLabel label = new JLabel();
         ImageIcon img = new ImageIcon(imgLocation);
         label.setIcon(img);
+
 //        label.setBounds(0,0,img.getIconWidth(),img.getIconHeight());
 //        mainPanel = new JPanel();
 //        mainPanel.setLayout();
